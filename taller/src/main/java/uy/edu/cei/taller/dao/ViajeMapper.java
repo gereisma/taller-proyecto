c4ba071b7f52eb6b7b516f5198b2c25efb91db53
GEREMIASCABRERA <gereisma17@hotmail.com>
2019-08-08 22:16:32 -0300

    viaje agregado

---
+++ taller/src/main/java/uy/edu/cei/taller/dao/ViajeMapper.java
@@ -0,0 +1,20 @@
+package uy.edu.cei.taller.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import uy.edu.cei.taller.bean.UsuarioBean;
+import uy.edu.cei.taller.bean.ViajeBean;
+
+@Mapper
+public interface ViajeMapper {
+
+	public List<ViajeBean> selectAll();
+	public ViajeBean selectById(@Param("id") long id);
+	public void insert(@Param("viaje") ViajeBean viaje);
+	public void updateById(@Param("usuario") ViajeBean viaje);
+	public void deleteById(@Param("id") int id);	
+		
+}
